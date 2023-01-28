297e3817801f3a7293fbdf02c8aee42f0e940c8c
leopoldtej <leopoldrocks535@gmail.com>
2023-01-28 15:43:16 -0600

    Added back the removed files from project 1

---
+++ Lab 1 - GPIO/Project/codegentemp/Lab1.ctl
@@ -0,0 +1,9 @@
+-- ======================================================================
+-- Lab1.ctl generated from Lab1
+-- 01/17/2023 at 23:16
+-- This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
+-- ======================================================================
+
+-- PSoC Clock Editor
+-- Directives Editor
+-- Analog Device Editor
--- Lab 1 - GPIO/Project/main.c
+++ Lab 1 - GPIO/Project/main.c
@@ -1,23 +1,12 @@
-/* ========================================
- *
- * Copyright YOUR COMPANY, THE YEAR
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
- *
- * ========================================
-*/
 #include "project.h"
-int printChar(char print, int length){
-    if(length == 16){
-        length = 0;
+int printChar(char print, int lengthOfDisplay){
+    if(lengthOfDisplay == 16){
+        lengthOfDisplay = 0;
         LCD_ClearDisplay();
     }
     LCD_WriteData(print);
-    length++;
-    return length;
+    lengthOfDisplay++;
+    return lengthOfDisplay;
 }
 int main(void)
 {
@@ -25,13 +14,14 @@ int main(void)
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     int x = 0;
-    int length = 0;
+    int lengthOfDisplay = 0;
     LCD_Start();
     LCD_ClearDisplay();
+    
+    
     for(;;)
     {
-        // Works
-        // Polling Process
+        // Check for press - polling
         while(C0_Read() == 0 && C1_Read() == 0 && C2_Read() == 0 && C3_Read() == 0){
             switch(x){
                 case 0:
@@ -55,94 +45,97 @@ int main(void)
                     x=0;
                     break;
             }
-        }
-        // End Polling
-        // Delay
+        } // Breakout of polling loop after a keypad press
+        
+        
+        // Delay for debouncing
         CyDelay(10);
-        // Do whatever
+        
+        
+        // Print whatever key is pressed, checking column by column.
         if(C0_Read() == 1){
             switch(x){
                 case 1:
                     // Print 1 (0,0)
-                    length = printChar('1', length);
+                    lengthOfDisplay = printChar('1', lengthOfDisplay);
                     break;
                 case 2:
-                    // Print 4 (0,1)
-                    length = printChar('4',length);
+                    // Print 4 (1,0)
+                    lengthOfDisplay = printChar('4',lengthOfDisplay);
                     break;
                 case 3:
-                    // Print 7 (0,2)
-                    length = printChar('7',length);
+                    // Print 7 (2,0)
+                    lengthOfDisplay = printChar('7',lengthOfDisplay);
                     break;
                 case 0:
-                    // Print * (0,3)
-                    length = printChar('*',length);
+                    // Print * (3,0)
+                    lengthOfDisplay = printChar('*',lengthOfDisplay);
                     break;
             }
         }
         else if(C1_Read() == 1){
             switch(x){
                 case 1:
-                    // Print 2 (1,0)
-                    length = printChar('2',length);
+                    // Print 2 (0,1)
+                    lengthOfDisplay = printChar('2',lengthOfDisplay);
                     break;
                 case 2:
                     // Print 5 (1,1)
-                    length = printChar('5',length);
+                    lengthOfDisplay = printChar('5',lengthOfDisplay);
                     break;
                 case 3:
-                    // Print 8 (1,2)
-                    length = printChar('8',length);
+                    // Print 8 (2,1)
+                    lengthOfDisplay = printChar('8',lengthOfDisplay);
                     break;
                 case 0:
-                    // Print 0 (1,3)
-                    length = printChar('0',length);
+                    // Print 0 (3,1)
+                    lengthOfDisplay = printChar('0',lengthOfDisplay);
                     break;
             }
         }
         else if(C2_Read() == 1){
             switch(x){
                 case 1:
-                    // Print 3 (2,0)
-                    length = printChar('3',length);
+                    // Print 3 (0,2)
+                    lengthOfDisplay = printChar('3',lengthOfDisplay);
                     break;
                 case 2:
-                    // Print 6 (2,1)
-                    length = printChar('6',length);
+                    // Print 6 (1,2)
+                    lengthOfDisplay = printChar('6',lengthOfDisplay);
                     break;
                 case 3:
                     // Print 9 (2,2)
-                    length = printChar('9',length);
+                    lengthOfDisplay = printChar('9',lengthOfDisplay);
                     break;
                 case 0:
-                    // Print # (2,3)
-                    length = printChar('#',length);
+                    // Print # (3,2)
+                    lengthOfDisplay = printChar('#',lengthOfDisplay);
                     break;
             }
         }
         else if(C3_Read() == 1){
             switch(x){
                 case 1:
-                    // Print A (3,0)
-                    length = printChar('A',length);
+                    // Print A (0,3)
+                    lengthOfDisplay = printChar('A',lengthOfDisplay);
                     break;
                 case 2:
-                    // Print B (3,1)
-                    length = printChar('B',length);
+                    // Print B (1,3)
+                    lengthOfDisplay = printChar('B',lengthOfDisplay);
                     break;
                 case 3:
-                    // Print C (3,2)
-                    length = printChar('C',length);
+                    // Print C (2,3)
+                    lengthOfDisplay = printChar('C',lengthOfDisplay);
                     break;
                 case 0:
                     // Print D (3,3)
-                    length = printChar('D',length);
+                    lengthOfDisplay = printChar('D',lengthOfDisplay);
                     break;
             }
         }
-        // Check for release
+        // Check for release - polling
         while(C0_Read() == 1 || C1_Read() == 1 || C2_Read() == 1 || C3_Read() == 1);
-        // Restart
+        // Restart the infinite for loop
     }
 }
 
